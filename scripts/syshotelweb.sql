fa5000fe17888b72c076a93807e99886abdf54ef
João Paulo <macieljp@outlook.com>
2018-12-10 02:00:07 -0200

    João Paulo

--- scripts/syshotelweb.sql
+++ scripts/syshotelweb.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: syshotelweb
 -- ------------------------------------------------------
--- Server version	5.7.24-0ubuntu0.18.04.1
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
